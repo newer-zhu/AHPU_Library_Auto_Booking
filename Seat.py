7f1993fff95a11ee2a220327420aef9f29a5edbc
newer-zhu <zhd30@outlook.com>
2020-09-15 10:31:39 +0800

    v1.0

--- Seat.py
+++ Seat.py
@@ -12,6 +12,8 @@ import datetime
 import time
 class Occupy(object):
     def __init__(self):
+        self.username = '3190112108'
+        self.password = '308532'
         self.now = datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d')
         #安徽工程大学图书馆楼层区域对应的roomId
         self.ahpu_lib = {
@@ -19,6 +21,8 @@ class Occupy(object):
             '5A': '47', '5B': '48',
             '5C': '49', '5D': '50', '6A': '51', '6B': '52', '6C': '53', '6D': '54', '6E': '55', '5S': '56'
         }
+        self.loginData = {'domain': 'seat.ahpu.edu.cn', 'httpOnly': True, 'name': 'JSESSIONID', 'path': '/tsgintf',
+                          'secure': False, 'value': 'F14952287EFA449AB69F3DAE46AC86D0'}
         # 被占座位
         self.seated = 0
         # 剩余座位
@@ -45,45 +49,66 @@ class Occupy(object):
         server.connect(smtp_server,465)
         server.login(from_addr,password)
         server.sendmail(from_addr,to_addr,msg.as_string())
+
     # 登录
     def login(self):
         browser = webdriver.Chrome()
         browser.get('http://seat.ahpu.edu.cn:9091/tsgintf/wechat/seat/html/reserving1.html?version=1.1')
-        wait = WebDriverWait(browser, 5)
+        wait = WebDriverWait(browser, 2)
+        # 登录
         try:
-            username = wait.until(EC.presence_of_element_located((By.ID, 'username')))
-            password = wait.until(EC.presence_of_element_located((By.ID, 'password')))
+            UN = wait.until(EC.presence_of_element_located((By.ID, 'username')))
+            PW = wait.until(EC.presence_of_element_located((By.ID, 'password')))
             button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '.auth_login_btn')))
         except NoSuchElementException:
             print("No Element!")
         try:
-            username.send_keys('3190112108')
-            password.send_keys('308532')
+            browser.execute_script("arguments[0].value = '" + self.username + "';", UN)
+            browser.execute_script("arguments[0].value = '" + self.password + "';", PW)
+            browser.execute_script("javascript:$('.iCheck-helper').click();")
             button.click()
         except Exception:
             print("Send Error!")
+            #设置Cookie
+        self.loginData.clear()
+        self.loginData = browser.get_cookie('JSESSIONID')
+        return browser
+
+    def set_Cookie(self):
+        browser = webdriver.Chrome()
+        browser.get('http://ids.ahpu.edu.cn/authserver/login?service=http%3A%2F%2Fxjwxt.ahpu.edu.cn%2Fahpu%2Flogin.action')
+        browser.add_cookie(self.loginData)
+        browser.get('http://seat.ahpu.edu.cn:9091/tsgintf/wechat/seat/html/reserving1.html?version=1.1')
+        return browser
+
+    # 选择
+    def choose(self,browser,begin,over):
+        lib = browser.find_element_by_id('libraryId')
+        date = browser.find_element_by_id('dateStrId')
+        type = browser.find_element_by_id('typeStrId')
+        start = browser.find_element_by_id('startHourStrId')
+        end = browser.find_element_by_id('endHourStrId')
+        button = browser.find_element_by_css_selector('.aui-btn')
+        browser.execute_script("arguments[0].value = '3';", lib)
+        browser.execute_script("arguments[0].value = '" + self.now + "';", date)
+        browser.execute_script("arguments[0].value = '1';", type)
+        browser.execute_script("arguments[0].value = '" + begin + "';", start)
+        browser.execute_script("arguments[0].value = '" + over + "';", end)
+        button.click()
         return browser
 
     def search(self,place,begin,over):
-        browser = self.login()
+        browser = self.set_Cookie()
         try:
-            lib = browser.find_element_by_id('libraryId')
-            date = browser.find_element_by_id('dateStrId')
-            type = browser.find_element_by_id('typeStrId')
-            start = browser.find_element_by_id('startHourStrId')
-            end = browser.find_element_by_id('endHourStrId')
-            button = browser.find_element_by_css_selector('.aui-btn')
-            browser.execute_script("arguments[0].value = '3';",lib)
-            browser.execute_script("arguments[0].value = '2020-9-13';",date)
-            browser.execute_script("arguments[0].value = '1';",type)
-            browser.execute_script("arguments[0].value = '"+begin+"';",start)
-            browser.execute_script("arguments[0].value = '"+over+"';",end)
-            button.click()
+            self.choose(browser,begin,over)
         except:
-            print('节点操作失败！')
+            print('登录过期！')
+            logined_browser = self.login()
+            self.set_Cookie()
+            browser = self.choose(logined_browser,begin,over)
 
         browser.execute_script("gotoReserving3Page("+self.ahpu_lib[place]+");")
-        time.sleep(1)
+        time.sleep(0.5)
         soup = BeautifulSoup(browser.page_source,'lxml')
         room_div = soup.find(attrs={'id': 'roomplanDiv'})
         for desk in room_div.children:
@@ -98,24 +123,18 @@ class Occupy(object):
                 else:
                     self.forbid += 1
         print("空闲座位{}个，不可坐座位{}个，被占座位{}个".format(self.left,self.forbid,self.seated))
-        # Cookies = {
-        #     'domain': 'seat.ahpu.edu.cn:9091',
-        #     'name': 'Cookie',
-        #     'value': 'JSESSIONID=5CC89802136333B8B7378A223D3BFD36',
-        #     'expires': '',
-        #     'path': '/',
-        #     'httpOnly': False,
-        #     'HostOnly': False,
-        #     'Secure': False
-        # }
-        # browser.add_cookie(Cookies)
-        # print(browser.page_source)
+        # browser.execute_script("doPreSeat('D51',roomId,dateStr, getStorageItem('userPhysicalCard'), startHour, endHour);")
+        print("success")
 
-
-
-
-
+        self.left = 0
+        self.forbid = 0
+        self.seated = 0
+        browser.close()
 
 if __name__ == '__main__':
     o = Occupy()
-    o.search('4A','14:00','16:00')
+    while True:
+        o.search('2A','19:00','20:00')
+        print('ok')
+    # print(b.get_cookies())
+        # time.sleep(2)
