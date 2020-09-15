3c33ab408d9595fbc005b644c29d0810284032b4
newer-zhu <zhd30@outlook.com>
2020-09-15 13:14:24 +0800

    v1.1

--- Seat.py
+++ Seat.py
@@ -1,4 +1,3 @@
-import requests
 from bs4 import BeautifulSoup
 import smtplib
 from email.mime.text import MIMEText
@@ -10,10 +9,14 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import datetime
 import time
-class Occupy(object):
-    def __init__(self):
-        self.username = '3190112108'
-        self.password = '308532'
+class Auto_Occupy(object):
+    def __init__(self,username,password):
+        # 浏览器对象
+        # option = webdriver.ChromeOptions
+        # option.headless
+        self.browser = webdriver.Chrome()
+        self.username = username
+        self.password = password
         self.now = datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d')
         #安徽工程大学图书馆楼层区域对应的roomId
         self.ahpu_lib = {
@@ -21,6 +24,7 @@ class Occupy(object):
             '5A': '47', '5B': '48',
             '5C': '49', '5D': '50', '6A': '51', '6B': '52', '6C': '53', '6D': '54', '6E': '55', '5S': '56'
         }
+        # 保持登录状态所需cookie
         self.loginData = {'domain': 'seat.ahpu.edu.cn', 'httpOnly': True, 'name': 'JSESSIONID', 'path': '/tsgintf',
                           'secure': False, 'value': 'F14952287EFA449AB69F3DAE46AC86D0'}
         # 被占座位
@@ -29,6 +33,8 @@ class Occupy(object):
         self.left = 0
         # 不可坐的
         self.forbid = 0
+        # 双人座
+        self.single = []
     # 发送邮件
     def sendEmail(self,text):
         # 发送者邮箱
@@ -52,9 +58,8 @@ class Occupy(object):
 
     # 登录
     def login(self):
-        browser = webdriver.Chrome()
-        browser.get('http://seat.ahpu.edu.cn:9091/tsgintf/wechat/seat/html/reserving1.html?version=1.1')
-        wait = WebDriverWait(browser, 2)
+        self.browser.get('http://seat.ahpu.edu.cn:9091/tsgintf/wechat/seat/html/reserving1.html?version=1.1')
+        wait = WebDriverWait(self.browser, 2)
         # 登录
         try:
             UN = wait.until(EC.presence_of_element_located((By.ID, 'username')))
@@ -63,55 +68,69 @@ class Occupy(object):
         except NoSuchElementException:
             print("No Element!")
         try:
-            browser.execute_script("arguments[0].value = '" + self.username + "';", UN)
-            browser.execute_script("arguments[0].value = '" + self.password + "';", PW)
-            browser.execute_script("javascript:$('.iCheck-helper').click();")
+            self.browser.execute_script("arguments[0].value = '" + self.username + "';", UN)
+            self.browser.execute_script("arguments[0].value = '" + self.password + "';", PW)
+            self.browser.execute_script("javascript:$('.iCheck-helper').click();")
             button.click()
         except Exception:
             print("Send Error!")
             #设置Cookie
         self.loginData.clear()
-        self.loginData = browser.get_cookie('JSESSIONID')
-        return browser
+        self.loginData = self.browser.get_cookie('JSESSIONID')
 
+    #设置cookie
     def set_Cookie(self):
-        browser = webdriver.Chrome()
-        browser.get('http://ids.ahpu.edu.cn/authserver/login?service=http%3A%2F%2Fxjwxt.ahpu.edu.cn%2Fahpu%2Flogin.action')
-        browser.add_cookie(self.loginData)
-        browser.get('http://seat.ahpu.edu.cn:9091/tsgintf/wechat/seat/html/reserving1.html?version=1.1')
-        return browser
+        self.browser.get('http://ids.ahpu.edu.cn/authserver/login?service=http%3A%2F%2Fxjwxt.ahpu.edu.cn%2Fahpu%2Flogin.action')
+        self.browser.add_cookie(self.loginData)
+        self.browser.get('http://seat.ahpu.edu.cn:9091/tsgintf/wechat/seat/html/reserving1.html?version=1.1')
 
-    # 选择
-    def choose(self,browser,begin,over):
-        lib = browser.find_element_by_id('libraryId')
-        date = browser.find_element_by_id('dateStrId')
-        type = browser.find_element_by_id('typeStrId')
-        start = browser.find_element_by_id('startHourStrId')
-        end = browser.find_element_by_id('endHourStrId')
-        button = browser.find_element_by_css_selector('.aui-btn')
-        browser.execute_script("arguments[0].value = '3';", lib)
-        browser.execute_script("arguments[0].value = '" + self.now + "';", date)
-        browser.execute_script("arguments[0].value = '1';", type)
-        browser.execute_script("arguments[0].value = '" + begin + "';", start)
-        browser.execute_script("arguments[0].value = '" + over + "';", end)
+    # 选择条件
+    def choose(self,begin,over):
+        lib = self.browser.find_element_by_id('libraryId')
+        date = self.browser.find_element_by_id('dateStrId')
+        type = self.browser.find_element_by_id('typeStrId')
+        start = self.browser.find_element_by_id('startHourStrId')
+        end = self.browser.find_element_by_id('endHourStrId')
+        button = self.browser.find_element_by_css_selector('.aui-btn')
+        self.browser.execute_script("arguments[0].value = '3';", lib)
+        self.browser.execute_script("arguments[0].value = '" + self.now + "';", date)
+        self.browser.execute_script("arguments[0].value = '1';", type)
+        self.browser.execute_script("arguments[0].value = '" + begin + "';", start)
+        self.browser.execute_script("arguments[0].value = '" + over + "';", end)
         button.click()
-        return browser
-
+    # 找空位
     def search(self,place,begin,over):
-        browser = self.set_Cookie()
+        self.set_Cookie()
+        # 默认执行循环
+        flag = True
         try:
-            self.choose(browser,begin,over)
+            self.choose(begin,over)
         except:
             print('登录过期！')
-            logined_browser = self.login()
+            self.login()
             self.set_Cookie()
-            browser = self.choose(logined_browser,begin,over)
+            self.choose(begin,over)
+        self.browser.execute_script("gotoReserving3Page("+self.ahpu_lib[place]+");")
+        # 加载页面
+        time.sleep(0.3)
+        soup = BeautifulSoup(self.browser.page_source,'lxml')
 
-        browser.execute_script("gotoReserving3Page("+self.ahpu_lib[place]+");")
-        time.sleep(0.5)
-        soup = BeautifulSoup(browser.page_source,'lxml')
         room_div = soup.find(attrs={'id': 'roomplanDiv'})
+        # 是否需要抢座，目的保证每次for循环都能执行
+        con = True
         for desk in room_div.children:
+            # 单人座抢座
+            if len(list(desk.children)) == 2:
+                for ds in desk.children:
+                    n = str(ds.attrs['class'])[-3:-2]
+                    print(n)
+                    if n == '3':
+                        loc = ds.contents[0].string
+                        self.single.append(loc)
+                        if con:
+                            flag = self.occupy(loc)
+                            con = False
+             # 统计
             for seat in desk.children:
                 s = str(seat.attrs['class'])
                 # 座位是否空闲看末尾的数字
@@ -122,19 +141,21 @@ class Occupy(object):
                     self.seated += 1
                 else:
                     self.forbid += 1
-        print("空闲座位{}个，不可坐座位{}个，被占座位{}个".format(self.left,self.forbid,self.seated))
-        # browser.execute_script("doPreSeat('D51',roomId,dateStr, getStorageItem('userPhysicalCard'), startHour, endHour);")
-        print("success")
+        print("空闲座位{}个，不可坐座位{}个，被占座位{}个，单人空闲座位{}个".format(self.left,self.forbid,self.seated,len(self.single)))
 
         self.left = 0
         self.forbid = 0
         self.seated = 0
-        browser.close()
+        self.single.clear()
+        return flag
 
-if __name__ == '__main__':
-    o = Occupy()
-    while True:
-        o.search('2A','19:00','20:00')
-        print('ok')
-    # print(b.get_cookies())
-        # time.sleep(2)
+    # 抢座,返回False说明不需要继续循环
+    def occupy(self,location):
+        try:
+            self.browser.execute_script("doPreSeat('"+location+"',roomId,dateStr, getStorageItem('userPhysicalCard'), startHour, endHour);")
+            self.sendEmail(""+location+"抢座成功!")
+            return False
+        except:
+            return True
+
+
